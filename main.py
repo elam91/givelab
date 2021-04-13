ddca013801cd1c2103b77f46046f7f42e730e53a
elam91 <elam91@gmail.com>
2021-04-13 13:54:44 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import openpyxl
+import time
+import schedule
+import random
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+
+def excel_to_dict(excel_path, headers=[]):
+    wb = openpyxl.load_workbook(excel_path)
+    sheet = wb['Sheet1']
+    result_dict = []
+    for row in range(2, sheet.max_row +1):
+        line = dict()
+        for header in headers:
+            cell_value = sheet.cell(column=(int(headers.index(header)+1)),row=int(row)).value
+            if type(cell_value) is int:
+                cell_value = str(cell_value)
+            elif cell_value is None:
+                cell_value = ''
+            line[header] = cell_value
+        result_dict.append(line)
+    return result_dict
+
+
+def go_to_givelab(username,user_password,giveaway_url):
+    options = Options()
+    options.headless = False
+    browser = webdriver.Firefox(options=options)
+    browser.get("https://givelab.com/login?ref=https%3A%2F%2Fgivelab.com%2F")
+
+    try:
+        element_present = EC.presence_of_element_located((By.ID, 'loginEmail'))
+        email = WebDriverWait(browser, 5).until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+    finally:
+        password = browser.find_element_by_id("loginPassword")
+        button = browser.find_element_by_id("loginSubmit")
+        email.send_keys(username)
+        password.send_keys(user_password)
+        time.sleep(2)
+        button.click()
+        time.sleep(2)
+        browser.get(giveaway_url)
+        time.sleep(2)
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, '//i[@class="fas fa-calendar-alt daily"]'))
+            daily_click = WebDriverWait(browser, 10).until(element_present)
+            daily_click.click()
+            time.sleep(2)
+            daily_click.click()
+            time.sleep(2)
+            browser.quit()
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+def daily_mission():
+    selhdrs=["email","password","url"]
+    task_list =  excel_to_dict('data.xlsx',selhdrs)
+    for task in task_list:
+        go_to_givelab(task["email"],task["password"],task["url"])
+        time.sleep(10)
+
+RANDOM_TIMES = ["08:00","08:01","08:02","08:03","08:04","08:05","08:06","08:07","08:08","08:09","08:10","08:11","08:12"]
+
+schedule.every().day.at(random.choice(RANDOM_TIMES)).do(daily_mission)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
